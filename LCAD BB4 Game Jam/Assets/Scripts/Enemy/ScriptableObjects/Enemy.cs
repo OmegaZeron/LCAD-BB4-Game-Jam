a8ece4c96fc2daafc31bff33e5e4781ae7998c77
DuskHour <DucNguyen013@gmail.com>
2018-09-30 14:23:33 -0700

    Enemy Scriptable Object

---
+++ LCAD BB4 Game Jam/Assets/Scripts/Enemy/ScriptableObjects/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enemy")]
+[System.Serializable]
+public class Enemy : ScriptableObject
+{
+    [SerializeField] private string m_name;
+    [SerializeField] private int m_health;
+    [SerializeField] private int m_damage;
+    [SerializeField] private int m_speed;
+    [SerializeField] private List<Ingredient> m_ingredient;
+}
