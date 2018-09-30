9997465fc42b7df876dd626a5449e667dde70fe6
Greg <omegazeron@yahoo.com>
2018-09-30 11:47:38 -0700

    Set up PlayerController

--- LCAD BB4 Game Jam/Assets/Scripts/Player/PlayerController.cs
+++ LCAD BB4 Game Jam/Assets/Scripts/Player/PlayerController.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    private Collider2D m_collider;
+    private Rigidbody2D m_rb;
+
+    private void Awake()
+    {
+        m_collider = GetComponent<BoxCollider2D>();
+        m_rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        m_rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update(){
 		
 	}
+
+    private void Jump()
+    {
+
+    }
 }
