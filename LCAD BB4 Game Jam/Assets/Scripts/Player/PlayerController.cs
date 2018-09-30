72a572dacea81d2c8d6ed3e92b7be186ec7261fe
PapaTrash <43727975+PapaTrash@users.noreply.github.com>
2018-09-30 12:07:55 -0700

    Added Movement to Player Controller

--- LCAD BB4 Game Jam/Assets/Scripts/Player/PlayerController.cs
+++ LCAD BB4 Game Jam/Assets/Scripts/Player/PlayerController.cs
@@ -8,12 +8,17 @@ public class PlayerController : MonoBehaviour {
 
     private Collider2D m_collider;
     private Rigidbody2D m_rb;
+    [SerializeField] private float m_speed = 10.0f;
+    [SerializeField] private KeyCode m_right;
+    [SerializeField] private KeyCode m_left;
 
     private void Awake()
     {
         m_collider = GetComponent<BoxCollider2D>();
-        m_rb.constraints = RigidbodyConstraints2D.FreezeRotation;
         m_rb = GetComponent<Rigidbody2D>();
+        m_rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        m_right = KeyCode.D;
+        m_left = KeyCode.A;
     }
 
     void Start()
@@ -21,12 +26,26 @@ public class PlayerController : MonoBehaviour {
 		
 	}
 	
-	void Update(){
-		
+	void Update()
+    {
+        Movement();
+        Jump();
 	}
 
     private void Jump()
     {
 
+    }
+
+    private void Movement()
+    {
+        if (Input.GetKey(m_right))
+        {
+            m_rb.velocity += Vector2.right * m_speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(m_left))
+        {
+            m_rb.velocity += Vector2.left * m_speed * Time.deltaTime;
+        }
     }
 }
