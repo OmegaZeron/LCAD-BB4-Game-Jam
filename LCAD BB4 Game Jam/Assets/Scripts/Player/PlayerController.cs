aa1fb2858f2daa901be7966e783d89492254f5c7
Greg <omegazeron@yahoo.com>
2018-09-30 12:10:15 -0700

    Added Jump features

--- LCAD BB4 Game Jam/Assets/Scripts/Player/PlayerController.cs
+++ LCAD BB4 Game Jam/Assets/Scripts/Player/PlayerController.cs
@@ -9,11 +9,17 @@ public class PlayerController : MonoBehaviour {
     private Collider2D m_collider;
     private Rigidbody2D m_rb;
 
+    [SerializeField] private float m_jumpForce = 4;
+    [SerializeField] private List<Transform> m_groundCheck;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private KeyCode m_jump;
+    [SerializeField] private KeyCode m_up;
+
     private void Awake()
     {
         m_collider = GetComponent<BoxCollider2D>();
-        m_rb.constraints = RigidbodyConstraints2D.FreezeRotation;
         m_rb = GetComponent<Rigidbody2D>();
+        m_rb.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 
     void Start()
@@ -21,12 +27,28 @@ public class PlayerController : MonoBehaviour {
 		
 	}
 	
-	void Update(){
-		
+	void Update()
+    {
+        Jump();
 	}
 
     private void Jump()
     {
-
+        if (Input.GetKeyDown(m_jump))
+        {
+            bool grounded = false;
+            foreach (Transform point in m_groundCheck)
+            {
+                RaycastHit2D hit = Physics2D.Linecast(point.position, point.position + Vector3.down * .05f, ground);
+                if (hit)
+                {
+                    grounded = true;
+                }
+            }
+            if (grounded)
+            {
+                m_rb.velocity += Vector2.up * m_jumpForce;
+            }
+        }
     }
 }
